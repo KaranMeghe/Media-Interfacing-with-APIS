e849b593c6206be629a9bc03ef9dc3f14f947fec
Karan Meghe <karanmeghe87@gmail.com>
2024-06-28 02:39:48 +0530

    feat: Thnks function is created , returned value of that Thunks function is using inside usersSlice

--- Media/src/Redux/Slices/usersSlice.js
+++ Media/src/Redux/Slices/usersSlice.js
@@ -1,9 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../Thunks/fetchUsers";
 
 const usersSlice = createSlice({
   name: "users",
-  initialState: { data: [] },
-  reducers: {},
+  initialState: {
+    isLoading: false,
+    data: [],
+    error: null,
+  },
+
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
 });
 
 export const userReducer = usersSlice.reducer;
