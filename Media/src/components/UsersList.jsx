82f23ab9365e326985a672f06165314d38f97be8
Karan Meghe <karanmeghe87@gmail.com>
2024-06-28 15:46:07 +0530

    refactor: updating states inside extraReducers

--- Media/src/components/UsersList.jsx
+++ Media/src/components/UsersList.jsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../Redux/Store";
+
 const UsersList = () => {
-  return "UsersList";
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  return "Loading....";
 };
 
 export default UsersList;
