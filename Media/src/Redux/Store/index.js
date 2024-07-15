711114c98155222565d70098a3dea1c9bd72ec3a
Karan Meghe <karanmeghe87@gmail.com>
2024-07-16 02:23:56 +0530

    add - made connection of the API to redux store, setup Reducer,middleware

--- Media/src/Redux/Store/index.js
+++ Media/src/Redux/Store/index.js
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { userReducer } from "../Slices/usersSlice";
+import { albumsApi } from "../Apis/albumApi";
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
+
+setupListeners(store.dispatch);
 
 export * from "../Thunks/fetchUsers";
 export * from "../Thunks/addUsers";
